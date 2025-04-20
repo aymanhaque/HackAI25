33c0c136d8651b943a1067fce0ebe8fb0f358e8e
FaaizS <faaiz.shaphy@gmail.com>
2025-04-20 09:11:30 -0500

    CSS and UI update

--- frontEnd/my-app/src/App.jsx
+++ frontEnd/my-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import prismLogo from './assets/prism.png' // Make sure to add your image
 
 function App() {
   const [messages, setMessages] = useState([])
@@ -85,7 +86,10 @@ function App() {
 
   return (
     <div className="chat-container">
-      <h1>PRISM</h1>
+      <div className="header">
+        <img src={prismLogo} alt="PRISM Logo" />
+        <h1>PRISM</h1>
+      </div>
 
       <div className="file-upload">
         <input
@@ -95,6 +99,10 @@ function App() {
           id="pdf-upload"
         />
         <label htmlFor="pdf-upload" className="upload-button">
+          <svg className="upload-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>
+            <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
+          </svg>
           Upload PDF
         </label>
         {selectedFile && (
